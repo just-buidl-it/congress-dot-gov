4047264b56f9156c132864b5b393a5bc62cff89b
Corey <coreyarice@gmail.com>
2025-06-11 10:05:22 -0300

    docs: improve docs for congress endpoints

--- src/clients/congress.ts
+++ src/clients/congress.ts
@@ -1,3 +1,7 @@
+/**
+ * @packageDocumentation
+ * Client for the `congress` endpoints.
+ */
 import { BaseClient } from './base';
 import type { PaginationParams, PaginatedResponse, BaseParams } from '../types';
 import type { CongressSummary, Congress } from '../schemas/congress';
@@ -9,8 +13,8 @@ export class CongressClient extends BaseClient {
 
   /**
    * Returns a list of congresses and congressional sessions.
-   * @param params {PaginationParams} - Pagination and format parameters
-   * @returns A list of congresses and their sessions
+   * @param params {@link PaginationParams} - Pagination and format parameters
+   * @returns A list of {@link CongressSummary congresses} and their sessions
    */
   async getCongresses(params: PaginationParams = {}) {
     return this.get<PaginatedResponse<{ congresses: CongressSummary[] }>>('', params);
@@ -19,8 +23,8 @@ export class CongressClient extends BaseClient {
   /**
    * Returns detailed information for a specified congress.
    * @param congress - The Congress number (e.g., 117)
-   * @param params {BaseParams} - Accepts format parameter (json or xml)
-   * @returns Detailed information for the specified congress
+   * @param params {@link BaseParams} - Accepts format parameter (json or xml)
+   * @returns {@link Congress Detail} information for the specified congress
    */
   async getCongress(congress: number, params: BaseParams = {}) {
     return this.get<{ congress: Congress }>(`/${congress}`, params);
@@ -28,8 +32,8 @@ export class CongressClient extends BaseClient {
 
   /**
    * Returns detailed information for the current congress.
-   * @param params {BaseParams} - Accepts format parameter (json or xml)
-   * @returns Detailed information for the current congress
+   * @param params {@link BaseParams} - Accepts format parameter (json or xml)
+   * @returns {@link Congress Detail} information for the current congress
    */
   async getCurrentCongress(params: BaseParams = {}) {
     return this.get<{ congress: Congress }>('/current', params);
